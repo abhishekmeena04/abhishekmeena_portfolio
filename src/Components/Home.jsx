10e2902871ba5ad666c964f1788109af010d361b
Abhishek Meena <abhishekmeena7452@gmail.com>
2024-08-16 09:59:22 +0530

    link-portfolio

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -263,13 +263,13 @@ const Home = () => {
               <div className="Mygitprojects">
                 <a
                   target="_blank"
-                  href="https://fitness-icon-gym-website.vercel.app"
+                  href="https://abhishekmeena-portfolio.vercel.app/"
                 >
                   <h2>fitness-icon-gym.com</h2>{" "}
                 </a>
                 <a
                   target="_blank"
-                  href="https://fitness-icon-gym-website.vercel.app"
+                  href="https://abhishekmeena-portfolio.vercel.app/"
                 >
                   <div className="projectimg1">
                     <img
